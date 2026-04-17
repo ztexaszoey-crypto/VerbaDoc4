dd9f0ea1eff1b51a118f89f183455afd6dd5be54
Zoey Ding <zoeydingry@icloud.com>
2026-04-17 13:35:43 -0500

    Initial Commit

---
+++ VerbaDoc4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VerbaDoc4
+//
+//  Created by Zoey Ding on 4/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VerbaDoc4/VerbaDoc4App.swift
@@ -0,0 +1,17 @@
+//
+//  VerbaDoc4App.swift
+//  VerbaDoc4
+//
+//  Created by Zoey Ding on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct VerbaDoc4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
