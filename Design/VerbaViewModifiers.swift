d8679c4d1dc9fc353e1588fcd0ea9e5a06b64dfb
Zoey Ding <zoeydingry@icloud.com>
2026-09-19 17:02:59 -0500

    Ignore Config.xcconfig so keys never get committed

---
+++ Design/VerbaViewModifiers.swift
@@ -0,0 +1,18 @@
+//
+//  VerbaViewModifiers.swift
+//  VerbaDoc
+//
+//  Created by Zoey Ding on 9/19/26.
+//
+
+import SwiftUI
+
+struct VerbaViewModifiers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VerbaViewModifiers()
+}
